5415894b8da86183f1d42a8789d5c590f61a5fc4
erroldmello <erroldmello2005@gmail.com>
2025-02-01 22:07:06 +0530

    Made select interactive to add multiple items to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -67,11 +67,15 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if(matchingItem) {
-        matchingItem.quantity += 1;
+        let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        let add = Number(quantitySelector.value);
+        matchingItem.quantity += add;
       } else {
+        let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        let add = Number(quantitySelector.value);
         cart.push({
           productId : productId,
-          quantity : 1
+          quantity : add
         });
       }
       
